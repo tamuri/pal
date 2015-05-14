236b315bdf9c46f081403c583fc2bdfaf3669669
Asif Tamuri <tamuri@gmail.com>
2015-05-14 15:29:38 +0100

    Remove makefiles (use ant)

--- src/makejar.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-rem ************************************************************
-rem This batch file makes an unsigned jar file
-rem ************************************************************
-@echo on 
-
-cd ..\classes
-
-jar cvf ..\pal.jar pal org *.class
